63ccd13e0783a72ba5e64d600fbc36279bb7cdb3
Zhen Yi Lim <zhen.yi@outlook.com>
2020-05-13 23:53:03 +0800

    switch away from using bootstrap cdn

--- client/src/index.js
+++ client/src/index.js
@@ -8,6 +8,7 @@ import { store } from "./redux/store";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 
+import "bootstrap/dist/css/bootstrap.min.css";
 import "./index.css";
 
 ReactDOM.render(
